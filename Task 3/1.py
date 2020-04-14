f6bbe41a0868eb0ddd9ba9caf4047bcfe2add624
Gandhali Kulkarni <gandhali4@gmail.com>
2020-04-13 21:14:00 -0700

    Create 1.py

---
+++ Task 3/1.py
@@ -0,0 +1,18 @@
+#1.	Create a list of the 10 elements of four different types of Data Type like int, string, complex and float.
+
+x =[1, 2, 'Gandhali', 5.08, 'tasks', 4+3j, 11.22, 35, 16+9j, 'Python']
+print(type(x))
+
+print(x[1])
+print(type(x[1]))
+
+print(x[2])
+print(type(x[2]))
+
+print(x[3])
+print(type(x[3]))
+
+print(x[5])
+print(type(x[5]))
+
+
